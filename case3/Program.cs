ff8d355307c9049612fb44c5204b7a2593457cec
Sergei <radchenkovsa@yandex.ru>
2023-08-30 11:26:09 +0300

    Case3

--- case3/Program.cs
+++ case3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!); 
+}
+
+int Fact(int num)
+{
+    int f = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+
+int number = InputNum("Введите число: ");
+int result = Fact(number);
+Console.WriteLine($"Факториал числа {number} равно {result}");
